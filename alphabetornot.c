132772004352148d545a528c7be3ef66dc208d4f
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-01-23 16:38:52 +0530

    Create alphabetornot.c

---
+++ alphabetornot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char a:
+clrscr();
+scanf(%c,"a");
+if(a>=='A' && a<=='Z');
+printf("the character is alphabet");
+else
+printf("the character is not alphabet")
+getch();
+}
