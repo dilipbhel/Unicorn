a62fc68048705c7f5b03d9e69f9a5d4b4225cdd2
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-01-23 16:52:35 +0530

    Create largnum.c

---
+++ largnum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+num a=44,b=54,c=34;
+clrscr();
+scanf("%n n% n%",&a,&b,&c);
+if(a>b && a>c){
+printf("print a is greater");}
+elseif(b>a && b>c){
+printf("print b is greater");}
+elseif(c>a && c>b){
+printf("print c is greater);}
+getch();
+}
