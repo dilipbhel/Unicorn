58084e2b711aefde8288189f362bd19d0f463062
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-01-23 16:24:57 +0530

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char c;
+clrscr();
+scanf("%c",&c)
+if(c=="a"||c=="e"||c=="i"||c=="O"||c=="u");
+printf("the character vowel");
+else
+printf("the character is consonent");
+getch();
+}
