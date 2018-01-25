d5b13ad0837a1a58120655c13d931dee53688e7a
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-01-25 16:44:36 +0530

    Create numinteger.c

---
+++ numinteger.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,N[i],c=0,T,K;
+printf(" get sum upto and no. of integers ");
+scanf("%d%d",&K,&T);
+for(i=1;i<=T;i++)
+scanf("%d",&N[i]);
+else
+for(i=1;i<=K;i++)
+c=c+N[i];
+printf(" %d is sum",c);
+getch();
+}
