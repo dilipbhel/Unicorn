0bd45fc98c971de478a14352c6ebff019e0efa72
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-01-25 16:21:37 +0530

    Create naturalnumbern.c

---
+++ naturalnumbern.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+scanf("%d",&n);
+if(n<0;n++);
+printf("the number is natural");
+else
+if(n>0;n++);
+printf("the number is not natural");
+getch();
+}
