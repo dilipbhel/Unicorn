7c774c566ec19d36b016b13e1b925c36fd38c8cd
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-02-06 16:06:01 +0530

    Create sum of AP.c

---
+++ sum of AP.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+     
+int main()
+{
+    int i,N,sum;
+     
+    /*read value of N*/
+    printf("Enter the value of N: ");
+    scanf("%d",&N);
+     
+    /*set sum by 0*/
+    sum=0;
+     
+    /*calculate sum of the series*/
+    for(i=1;i<=N;i++)
+        sum= sum+ i;
+        printf("Sum of the series is: %d\n",sum);
+        getch();
+}
