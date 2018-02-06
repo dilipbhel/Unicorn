24612eed31c4eeefa7941daea9109db0f812cde6
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-02-06 16:18:11 +0530

    Create Max element.C

---
+++ Max element.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int o,b[20],n,j;
+printf("\n Enter the number");
+scanf("%d",&o);
+printf("\n Enter the numbers");
+for(i=1;i<n;i++);
+scanf("%d",&b[n]);
+j=b[0];
+for(i=1;i<n;i++);
+{
+if(b[n]>j)
+j=b[n];
+}
+printf("\n The value is %d",j);
+}
