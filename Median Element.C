443aa41e499aa64b8c5c2eed776bd8fa10b8eaf9
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-02-06 16:31:14 +0530

    Create Median Element.C

---
+++ Median Element.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,j,t;
+clrscr();
+printf("enter the value");
+scanf("%d",&n);
+printf("input");
+int a[n];
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+printf("median is:%d",a[(n+1/2)]);
+getch();
+}
