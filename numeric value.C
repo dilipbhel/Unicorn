bb29c5a9497a0b2dd336996936f42a77cb160620
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-02-08 16:14:36 +0530

    Create numeric value.C

---
+++ numeric value.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the value");
+scanf("%d",&n);
+if(n>='0' || n<='9');
+printf("\n %d is yes",n);
+else
+printf("\n %d is no",n);
+getch();
+}
