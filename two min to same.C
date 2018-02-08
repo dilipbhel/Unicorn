292fbc3627bed02251a621caa28f66de60a111c0
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-02-08 16:41:51 +0530

    Create two min to same.C

---
+++ two min to same.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int h,m,h1,m1,e,f;
+printf("enter the first value");
+scanf("%d%d",&h,&m);
+printf("enter the second value\n");
+scanf("%d",&h1,&m1);
+h1=h-m;
+f=m1-e;
+printf("%d=%d",h1);
+printf("%d=%d",m1);
+}
+return 0;
+}
