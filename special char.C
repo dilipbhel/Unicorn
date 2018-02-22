d121db425c3abe528728d8dd34923152a43b7e00
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-02-22 16:35:41 +0530

    Update special char.C

--- special char.C
+++ special char.C
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
-int main(void) 
+#include<conio.h>
+void main()
 {
-	char str[50];
+	char str[55];
 	int i,count=0;
 	printf("enter the sentence:");
 	scanf("%[^\n]s",str);
@@ -13,6 +13,6 @@ int main(void)
 			count=count+1;
 		}
 	}
-	printf("\nthe number of special characters in the sentence is:%d",count);
-	return 0;
+	printf("\n number of special characters in the sentence is %d",count);
+	getch();
 }
