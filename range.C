306c911f605a72118bc01e6493082b95bba9f34f
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-03-02 15:06:48 +0530

    Create range.C

---
+++ range.C
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+ void main()
+ {
+    int a;
+    printf(" the values are");
+    scanf("%d",&a);
+    if(a>=1 && a<=10)
+    printf(" YES",a);
+    else
+    printf(" NO",a);
+ }
