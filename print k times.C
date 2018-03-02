db691125fd51f5362390f0087d5f9b544aacde60
dilipbhel <35652079+dilipbhel@users.noreply.github.com>
2018-03-02 14:18:05 +0530

    Create print k times.C

---
+++ print k times.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  char str[20];
+   int i,n;
+    printf(" Enter the name");
+    scanf("%s %d",str,&n);
+    for(i=1;i<=n;i++)
+    printf("\n the result is %s",str);
+    
+        
+    }
